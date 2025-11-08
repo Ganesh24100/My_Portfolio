e99f8989ba263c50aa8d3b96206c5fb59287c258
ganesh-pravi <ganesh@pravi.co>
2025-11-08 12:03:57 +0530

    first

---
+++ src/components/section/Contact.jsx
@@ -0,0 +1,165 @@
+import React, { useRef } from "react";
+import styled from "styled-components";
+import emailjs from "@emailjs/browser";
+import { toast } from "react-toastify";
+import { LuSend } from "react-icons/lu";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-contnet: center;
+  position: rlative;
+  z-index: 1;
+  align-items: center;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  max-width: 1100px;
+  gap: 12px;
+  @media (max-width: 960px) {
+    flex-direction: column;
+  }
+`;
+const Title = styled.div`
+  font-size: 52px;
+  text-align: center;
+  font-weight: 600;
+  margin-top: 20px;
+  color: ${({ theme }) => theme.text_primary};
+  @media (max-width: 768px) {
+    margin-top: 12px;
+    font-size: 32px;
+  }
+`;
+const Desc = styled.div`
+  font-size: 18px;
+  text-align: center;
+  font-weight: 600;
+  color: ${({ theme }) => theme.text_secondary};
+  @media (max-width: 768px) {
+    font-size: 16px;
+  }
+`;
+
+const ContactForm = styled.form`
+  width: 95%;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.contact};
+  border: 1px solid rgba(255, 255, 255, 0.125);
+  padding: 32px;
+  border-radius: 12px;
+  box-shadow: rgba(23, 92, 230, 0.1) 0px 4px 24px;
+  margin-top: 28px;
+  gap: 12px;
+`;
+const ContactTitle = styled.div`
+  font-size: 28px;
+  margin-bottom: 6px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.text_primary};
+`;
+const ContactInput = styled.input`
+  required;
+  flex: 1;
+  background-color: transparent;
+  border: 1px solid ${({ theme }) => theme.text_secondary + 50};
+  outline: none;
+  font-size: 18px;
+  color: ${({ theme }) => theme.text_primary};
+  border-radius: 12px;
+  padding: 12px 16px;
+  &:focus {
+    border: 1px solid ${({ theme }) => theme.primary};
+  }
+`;
+const ContactInputMessage = styled.textarea`
+  required
+  flex: 1;
+  background-color: transparent;
+  border: 1px solid ${({ theme }) => theme.text_secondary + 50};
+  outline: none;
+  font-size: 18px;
+  color: ${({ theme }) => theme.text_primary};
+  border-radius: 12px;
+  padding: 12px 16px;
+  &:focus {
+    border: 1px solid ${({ theme }) => theme.primary};
+  }
+`;
+const ContactButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+  width: 100%;
+  text-decoration: none;
+  text-align: center;
+  background: hsla(271, 100%, 50%, 1);
+  padding: 13px 16px;
+  border-radius: 12px;
+  cursor: pointer;
+  border: none;
+  color: white;
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+export const Contact = () => {
+  const forms = useRef();
+  const handelSubmit = async (e) => {
+    const toastId = toast.loading("Loading...");
+    e.preventDefault();
+    
+       try {
+    await emailjs.sendForm(
+     "service_zdfmi6b",
+        "template_ppsvkbc",
+        forms.current,
+        "lyURxa50imSv2LiyO"
+    );
+    forms.current.reset();
+    toast.success("Email Sent!");
+  } catch (error) {
+    console.log(error);
+    toast.error("Unable to send Email!");
+  } finally {
+    toast.dismiss(toastId);
+  }
+  };
+  return (
+    <Container id="Contact">
+      <Wrapper>
+        <Title>Contact</Title>
+        <Desc
+          style={{
+            marginBottom: "40px",
+          }}
+        >
+          Feel free to reach out to me for any questions or opportunities!
+        </Desc>
+        <ContactForm ref={forms} onSubmit={handelSubmit}>
+          <ContactTitle>Email Me 🚀</ContactTitle>
+          <ContactInput placeholder="Your Email" name="from_email" required />
+          <ContactInput placeholder="Your Name" name="from_name" required />
+          <ContactInputMessage
+            required
+            placeholder="Message"
+            name="message"
+            rows={4}
+          />
+          <ContactButton value="Send">
+            Send Mail <LuSend />
+          </ContactButton>
+        </ContactForm>
+      </Wrapper>
+    </Container>
+  );
+};
